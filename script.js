fae52adc2f1da092cdc1bd8c77b1ac0f8e7d4ad1
sejpalsinh jadeja <59164285+sejpalsinh@users.noreply.github.com>
2020-06-29 12:26:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ drone.on('open', error => {
   // connected to the room (including us). Signaling server is ready.
   room.on('members', members => {
     console.log('MEMBERS', members);
+    console.log("updated updated");
     // If we are the second user to connect to the room we will be creating the offer
     const isOfferer = members.length === 2;
     startWebRTC(isOfferer);
